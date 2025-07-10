21db811f9b7c5ccc1ffebe8da4edf193ed030bfc
Alexander <lanxel97@gmail.com>
2025-07-10 02:16:01 -0500

    First web page design

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        accent: "#f0b570",
+        // Optionally, you can add secondary: '#ffffff' if needed
+      },
+    },
+  },
+  plugins: [],
+}
