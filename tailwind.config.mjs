87077e80be921df7268e5edf52d825fb20f1a5b8
Alexander <lanxel97@gmail.com>
2025-07-21 00:22:43 -0500

    Fix font, colors and responsive view

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        'anaktoria': ['Anaktoria', 'sans-serif'],
+      },
       colors: {
         primary: "#000000",
         accent: "#f0b570",
